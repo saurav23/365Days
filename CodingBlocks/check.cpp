7867e57e4d7cfae030beb736401e262dd688189c
saurav23 <sauravtarun813@gmail.com>
2021-04-02 03:39:32 +0530

    Update

--- CodingBlocks/check.cpp
+++ CodingBlocks/check.cpp
@@ -5,21 +5,33 @@ using namespace std;
 // mian
 int main()
 {
-    vector<int *> num;
+    vector<int> num;
 
     // cout << num ;
     int n;
     cin >> n;
+    int input;
     for (int i = 0; i < n; i++)
     {
-        num.push_back(&i);
+        cin >> input;
+        num.push_back(input);
     }
+
+    cout << "Size:" << num.size() << endl;
+    // cout << "0th:" << num[0] << endl;
+    // cout << "1th:" << num[1] << endl;
+    // cout << "2th:" << num[2] << endl;
+    // cout << "3th:" << num[3] << endl;
 
     // cout << num[] ;
-    for (int i = 0; i < num.size(); i++)
-    {
-        // cout <<  << endl ;
-    }
+    for (int i = 0; i < num.size(); i++){
+        // {   if(i == (num.size()-1)){
+        //         cout << num[i] ;
+        //     }
+        //     if(i<(num.size()-1))
+        //     cout << num[i] << " -> ";
+        cout << num[i] << "   ";
+}
 
-    cout << endl;
+cout << endl;
 }
---
+++ CodingBlocks/findMajority.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// solution
+vector<int> majorityElements(vector<int> vec)
+{
+    int element1 = vec[0];
+    int count1 = 1;
+
+    int element2 = 0;
+    int count2 = 0;
+
+    for (int i = 1; i < vec.size(); i++)
+    {
+
+        if (element1 == vec[i])
+        {
+            count1++;
+        }
+        else if (element2 == vec[i])
+        {
+            count2++;
+        }
+        else if (count1 == 0)
+        {
+            element1 = vec[i];
+            count1 = 1;
+        }
+        else if (count2 == 0)
+        {
+            element2 = vec[i];
+            count2 = 1;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = count2 = 0;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] == element1)
+        {
+            count1;
+        }
+        else if (vec[i] == element2)
+        {
+            count2;
+        }
+    }
+
+    vector<int> ans;
+    if (count1 > vec.size() / 3)
+    {
+        ans.push_back(element1);
+    }
+    if (count2 > vec.size() / 3)
+    {
+        ans.push_back(element2);
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> vec;
+    int input;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input;
+        vec.push_back(input);
+    }
+
+    vector<int> ans;
+
+    ans = majorityElements(vec);
+
+    if (ans.size() == 0)
+    {
+        cout << "No Majority Elements";
+    }
+    else
+    {
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
