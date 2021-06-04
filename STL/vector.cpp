2c443d57c28ef73d08e1edad84cefe56a2a41856
saurav23 <sauravtarun813@gmail.com>
2021-06-04 15:57:28 +0530

    update

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -11,10 +11,29 @@ int main()
     }
     cout << endl;
     vector<int>::iterator it1 = vect.begin();
-    cout << (*it1)<< endl ;
+    // cout << (*it1) << endl; // value at it1
 
-    cout << &it1 << endl ;
+    // cout << &it1 << endl; // address of it1
 
+    for (it1 = vect.begin(); it1 != vect.end(); ++it1)
+    {
+        cout << (*it1) << " ";
+    }
+    cout << endl;
+    // Example 2
+    vector<pair<int, int>> vp = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+    vector<pair<int, int>>::iterator it2;
+    for (it2 = vp.begin(); it2 != vp.end(); ++it2)
+    {
+        cout << (*it2).first << " " << (*it2).second << endl; // (*it2).first = it2->first
+    }
+
+    cout << endl
+         << "-------------------------------" << endl;
+    for (it2 = vp.begin(); it2 != vp.end(); ++it2)
+    {
+        cout << it2->first << " " << it2->second << endl; // (*it2).first = it2->first
+    }
 
     // ----------------------------
     cout << endl;
